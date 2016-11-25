b4d8cbdd49693b75aed8a6304db05bdf093dde7f
kloik <kloikfriendq@gmail.com>
2016-11-25 17:20:10 +0530

    Plane flying good

--- core/src/ga/gauravchauhan/samplegame/SampleGame.java
+++ core/src/ga/gauravchauhan/samplegame/SampleGame.java
@@ -5,20 +5,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class SampleGame extends ApplicationAdapter {
 
     private final static Vector2 damping = new Vector2(.99f, .99f);
+    private static final float TAP_DRAW_TIME_MAX = 1f;
+    private static final float TOUCH_IMPULSE = 500;
 
+    Viewport viewport;
     SpriteBatch batch;
     FPSLogger fpsLogger;
     OrthographicCamera camera;
-    Texture background;
+    TextureAtlas.AtlasRegion bgRegion;
     TextureRegion terrainBelow;
     TextureRegion terrainAbove;
     Animation plane;
@@ -28,6 +35,11 @@ public class SampleGame extends ApplicationAdapter {
     Vector2 planePosition = new Vector2();
     Vector2 planeDefaultPosition = new Vector2();
     Vector2 gravity = new Vector2();
+    TextureAtlas atlas;
+    private Vector3 touchPosition = new Vector3();
+    private Vector2 tmpVector = new Vector2();
+    private float tapDrawTime;
+    private TextureRegion tapIndicator;
 
     @Override
     public void create() {
@@ -35,22 +47,31 @@ public class SampleGame extends ApplicationAdapter {
         fpsLogger = new FPSLogger();
         // img = new Texture("badlogic.jpg");
 
+        atlas = new TextureAtlas(Gdx.files.internal("ThrustCopter.pack"));
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
+        viewport = new FitViewport(800, 480, camera);
 
-        background = new Texture("background.png");
-        terrainBelow = new TextureRegion(new Texture("groundGrass.png"));
+        tapIndicator = atlas.findRegion("tap2");
+        bgRegion = atlas.findRegion("background");
+        terrainBelow = atlas.findRegion("groundGrass");
         terrainAbove = new TextureRegion(terrainBelow);
         terrainAbove.flip(true, true);
 
         plane = new Animation(.05f,
-                new TextureRegion(new Texture("planeRed1.png")),
-                new TextureRegion(new Texture("planeRed2.png")),
-                new TextureRegion(new Texture("planeRed3.png"))
+                atlas.findRegion("planeRed1"),
+                atlas.findRegion("planeRed2"),
+                atlas.findRegion("planeRed3")
         );
         plane.setPlayMode(Animation.PlayMode.LOOP);
 
         resetScene();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 
     @Override
@@ -64,11 +85,37 @@ public class SampleGame extends ApplicationAdapter {
 
     private void updateScene() {
         final float deltaTime = Gdx.graphics.getDeltaTime();
-        terrainOffset -= 200 * deltaTime; // x0 - 200dt;
+
+        if (Gdx.input.justTouched()) {
+            touchPosition.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPosition);
+            // touch position done
+
+            tmpVector.set(planePosition.x, planePosition.y);
+            tmpVector.sub(touchPosition.x, touchPosition.y).nor();
+            planeVelocity.mulAdd(tmpVector,
+                    TOUCH_IMPULSE - MathUtils.clamp(Vector2.dst(touchPosition.x, touchPosition.y,
+                            planePosition.x, planePosition.y), 0, TOUCH_IMPULSE));
+            tapDrawTime = TAP_DRAW_TIME_MAX;
+        }
+        tapDrawTime -= deltaTime;
+
         planeAnimTime += deltaTime;
 
+        planeVelocity.scl(damping);
         planeVelocity.add(gravity);
-        planeVelocity.mulAdd(planeVelocity, deltaTime);
+        planePosition.mulAdd(planeVelocity, deltaTime);
+
+        terrainOffset -= planePosition.x - planeDefaultPosition.x;
+
+        if (-terrainOffset > terrainBelow.getRegionWidth()) {
+            terrainOffset = 0;
+        }
+
+        if (terrainOffset > 0) {
+            terrainOffset -= terrainBelow.getRegionWidth();
+        }
+
     }
 
     private void drawScene() {
@@ -77,7 +124,7 @@ public class SampleGame extends ApplicationAdapter {
 
         batch.begin();
         batch.disableBlending();
-        batch.draw(background, 0, 0);
+        batch.draw(bgRegion, 0, 0);
         batch.enableBlending();
 
         batch.draw(terrainBelow, terrainOffset, 0);
@@ -85,14 +132,19 @@ public class SampleGame extends ApplicationAdapter {
         batch.draw(terrainAbove, terrainOffset, 480 - terrainAbove.getRegionHeight());
         batch.draw(terrainAbove, terrainOffset + terrainBelow.getRegionWidth(), 480 - terrainAbove.getRegionHeight());
         batch.draw(plane.getKeyFrame(planeAnimTime), planePosition.x, planePosition.y);
+
+        if (tapDrawTime > 0) {
+            batch.draw(tapIndicator, touchPosition.x - 29.5f, touchPosition.y - 29.5f);
+        }
+
         batch.end();
     }
 
     private void resetScene() {
         terrainOffset = 0;
         planeAnimTime = 0;
-        planeVelocity.set(0, 0);
-        gravity.set(0, -2);
+        planeVelocity.set(10, 0);
+        gravity.set(0, -4);
         planeDefaultPosition.set(400 - 88 / 2, 240 - 73 / 2);
         planePosition.set(planeDefaultPosition.x, planeDefaultPosition.y);
     }
@@ -100,6 +152,6 @@ public class SampleGame extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
-        background.dispose();
+        atlas.dispose();
     }
 }
