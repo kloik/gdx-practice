f09f03946e4dac45bfa3b07b08a0d669c17dce4a
kloik <kloikfriendq@gmail.com>
2016-11-25 12:40:28 +0530

    init

---
+++ desktop/src/ga/gauravchauhan/samplegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ga.gauravchauhan.samplegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ga.gauravchauhan.samplegame.SampleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SampleGame(), config);
+	}
+}
