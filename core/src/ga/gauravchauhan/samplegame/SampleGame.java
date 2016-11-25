4b6683faec6ca3237bed3b35e916b00b53d92e0b
kloik <kloikfriendq@gmail.com>
2016-11-25 14:39:50 +0530

    Hello world

--- core/src/ga/gauravchauhan/samplegame/SampleGame.java
+++ core/src/ga/gauravchauhan/samplegame/SampleGame.java
@@ -2,32 +2,35 @@ package ga.gauravchauhan.samplegame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SampleGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    BitmapFont font;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont(Gdx.files.internal("verdana39.fnt"),
+                Gdx.files.internal("verdana39.png"), false);
+        font.setColor(Color.RED);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        font.draw(batch, "MyText", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        batch.end();
+    }
 }
