025c0b38b774c3b0c069fb73500d2018b1d2384d
kloik <kloikfriendq@gmail.com>
2016-11-25 16:05:52 +0530

    Added dimensions of screen

--- desktop/src/ga/gauravchauhan/samplegame/desktop/DesktopLauncher.java
+++ desktop/src/ga/gauravchauhan/samplegame/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package ga.gauravchauhan.samplegame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import ga.gauravchauhan.samplegame.SampleGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SampleGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 480;
+        new LwjglApplication(new SampleGame(), config);
+    }
 }
